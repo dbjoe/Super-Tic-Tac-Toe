2b37393739602f873147616b5655e370a8c674b1
dbjoe <josephzylla@gmail.com>
2022-10-11 23:36:03 -0400

    Super

--- SuperTicTacToe.java
+++ SuperTicTacToe.java
@@ -6,13 +6,13 @@ import java.awt.*;
 public class SuperTicTacToe {
 
 	public static void main(String[] args) {
-		int boardSize = 3;
-		int numToWin = 3;
+		int boardSize = 4;
+		int numToWin = 4;
 		boolean isXFirst = true;
 		String boardInput;
 		String numInput;
 		String isXFirstInput;
-
+		
 		boolean boardLoopAgain;
 		do {
 			// set loop condition to false to prevent infinite loop
@@ -26,14 +26,20 @@ public class SuperTicTacToe {
 				boardSize = Integer.parseInt(boardInput);
 			} catch (NumberFormatException e) {
 				boardLoopAgain = true;
+				JOptionPane.showMessageDialog(null, "Please enter an integer between 3 and 15");
 			}
 
-			if (boardSize < 3 || boardSize > 15)
+			if (boardSize < 3 || boardSize > 15) {
 				boardLoopAgain = true;
+				JOptionPane.showMessageDialog(null, "Please enter an integer between 3 and 15");
+			}
+				
 		} while (boardLoopAgain);
 
 		if (boardSize == 3)
 			numToWin = 3;
+		else if (boardSize == 4)
+			numToWin = 4;
 		else {
 			boolean numLoopAgain;
 			do {
@@ -47,22 +53,30 @@ public class SuperTicTacToe {
 					numToWin = Integer.parseInt(numInput);
 				} catch (NumberFormatException e) {
 					numLoopAgain = true;
+					JOptionPane.showMessageDialog(null, "Please enter an integer between 4 and " + boardSize);
 				}
 
-				if (numToWin < 4 || numToWin > boardSize)
+				if (numToWin < 4 || numToWin > boardSize) {
 					numLoopAgain = true;
+					JOptionPane.showMessageDialog(null, "Please enter an integer between 4 and " + boardSize);
+				}
+					
 			} while (numLoopAgain);
 		}
 
-		// TODO: fix so that hitting cancel does not throw NullPointerException
 		isXFirstInput = JOptionPane.showInputDialog(null, "Who starts first? X or O");
-		if (isXFirstInput.equalsIgnoreCase("o"))
-			isXFirst = false;
-		else if (isXFirstInput.equalsIgnoreCase("x"))
-			isXFirst = true;
+		if (isXFirstInput == null) {
+			System.exit(0);
+		}
 		else {
-			JOptionPane.showMessageDialog(null, "Unexpected input. " + "X will start first");
-			isXFirst = true;
+			if (isXFirstInput.equalsIgnoreCase("o"))
+				isXFirst = false;
+			else if (isXFirstInput.equalsIgnoreCase("x"))
+				isXFirst = true;
+			else {
+				JOptionPane.showMessageDialog(null, "Unexpected input. X will start first.");
+				isXFirst = true;
+			}
 		}
 
 		SuperTicTacToeGame game = new SuperTicTacToeGame(boardSize, numToWin, isXFirst);
